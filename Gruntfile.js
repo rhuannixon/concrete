0c1aacc418bf15b8017ebfb0f86b84b80e2783ea
Rhuan Dantas <rhuannixon@gmail.com>
2019-11-04 23:02:46 -0300

    create grunt.js

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: ['Gruntfile.js', './**/*.js']
+        }
+      });
+  
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+  
+    // Default task(s).
+    grunt.registerTask('default', ['jshint']);
+  
+  };
