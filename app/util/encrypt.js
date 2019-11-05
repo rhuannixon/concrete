8f73ec360aac157decf1008ecc3c79f6b8db9036
Rhuan Dantas <rhuannixon@gmail.com>
2019-11-05 00:01:10 -0300

    change root path

--- app/util/encrypt.js
+++ app/util/encrypt.js
@@ -2,9 +2,9 @@ const bcrypt = require("bcrypt");
 
 exports.generateHash = function(password){
     return bcrypt.hash(password, bcrypt.genSaltSync(8));
-}
+};
 
 
 exports.validPassword = function(passed,existing){
     return bcrypt.compare(passed, existing);
-}
+};
