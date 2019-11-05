47880b08cf00d7a4be458680e5400781bbd5cc07
Rhuan Dantas <rndantas@latam.stefanini.com>
2019-11-05 13:12:13 -0300

    change method name

--- app/util/encrypt.js
+++ app/util/encrypt.js
@@ -1,10 +1,10 @@
 const bcrypt = require("bcrypt");
 
-exports.generateHash = function(password){
+exports.generateHash = function(password) {
     return bcrypt.hash(password, bcrypt.genSaltSync(8));
 };
 
 
-exports.validPassword = function(passed,existing){
-    return bcrypt.compare(passed, existing);
+exports.validPassword = function(passed, existing) {
+    return bcrypt.compareSync(passed, existing);
 };
