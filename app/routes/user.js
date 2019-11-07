dc51c488574a3243782487e7e8242892cf3eb165
Rhuan Dantas <rhuannixon@gmail.com>
2019-11-06 22:34:30 -0300

    use try/catch

--- app/routes/user.js
+++ app/routes/user.js
@@ -4,26 +4,33 @@ const auth = require('../util/auth');
 const encrypt = require('../util/encrypt');
 const { User, Telefone } = require('../models/index');
 
-router.post('/signup', (req, res, next) => {
-    newUser = req.body;
-    User.create(newUser)
-        .then(async user => {
-            token = await auth.generateAuthToken(user.id);
-            user.token = token;
-
-            user.save();
-            res.setHeader('Bearer', token);
-            res.status(201).json(user);
-        })
-        .catch(err => res.status(500).send(err));
+router.post('/signup', async (req, res, next) => {
+    try{
+        const newUser = await User.create(req.body);    
+        token = await auth.generateAuthToken(newUser.id);
+        newUser.token = token;
+        
+        for(var i = 0;i<req.body.telefones.length;i++)
+        {
+            var {numero,ddd} = req.body.telefones[i];
+            console.log(`${numero}: ${ddd}`);
+            await Telefone.create({
+                "numero":numero,
+                "ddd":ddd,
+                "userId":newUser.id})
+                .catch(err => console.log(err));
+        }
+        newUser.save();
+        res.setHeader('Bearer', token);
+        res.status(201).json(newUser);
+    }catch(err){
+        res.status(500).send(err);
+    }
 });
 
-router.post('/signin', (req, res, next) => {
-    User.findOne({
-        where: {
-            email: req.body.email
-        }
-    }).then(async user => {
+router.post('/signin', async (req, res, next) => {
+    try{
+        user = await User.findOne({email: req.body.email});
         if (user) {
             isValid = encrypt.validPassword(req.body.password, user.password);
             if (isValid) {
@@ -34,24 +41,31 @@ router.post('/signin', (req, res, next) => {
                 return res.status(200).json(user);
             }
         }
-        return res.status(401).json("Usuário e/ou senha inválidos")
-    }).catch(err => res.status(500).send(err));
+        return res.status(401).json("Usuário e/ou senha inválidos");
+    }catch(err){
+        console.log(err);
+        res.status(500).send(err);
+    }
 });
 
-router.get('/search/:id', (req, res, next) => {
-    var token = req.headers['bearer'];
-    if (!token) return res.status(401).send('Não autorizado');
+router.get('/search/:id', async (req, res, next) => {
+    try{
+        var token = req.headers.bearer;
+        if (!token) return res.status(401).send('Não autorizado');
 
-    auth.verify(req, res, token)
+        auth.verify(req, res, token);
 
-    User.findOne({
-            where: {
-                id: req.params.id
-            },
-            include: ['Telefones']
-        })
-        .then(user => res.status(200).send(user))
-        .catch(err => res.status(500).send({ error: err }));
+        user = await User.findOne({
+                where: {
+                    id: req.params.id
+                },
+                include: ['Telefones']
+                });
+        res.status(200).send(user);
+    }catch(err){
+        console.log(err);
+        res.status(500).send(err);
+    }
 });
 
 module.exports = router;
