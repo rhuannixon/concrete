9592934be2f970b7ce2d70469fd67b00baea7761
Rhuan <rhuannixon@gmail.com>
2020-04-08 23:42:07 -0300

    parse env expires to int

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('dotenv/config');
 
-const expiresIn = process.env.TOKEN_EXPIRES || 1800;
+const expiresIn = parseInt(process.env.TOKEN_EXPIRES) || 1800;
 const secrete = process.env.API_KEY;
 
 const generateAuthToken = (id) => jwt.sign({ id }, secrete, { expiresIn });
